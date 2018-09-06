5686f6d844b651d6180f7c51ff957820f9cf5b1f
Mao Ning <chemaon@nus.edu.sg>
2018-09-06 21:26:54 +0800

    add alignSelf to the height of the image.  To remove the height becomes very long when working with bootstrap of web2py

--- src/Lightbox.js
+++ src/Lightbox.js
@@ -434,6 +434,7 @@ const defaultStyles = {
 		height: 'auto',
 		margin: '0 auto', // maintain center on very short screens OR very narrow image
 		maxWidth: '100%',
+                alignSelf: 'center',
 
 		// disable user select
 		WebkitTouchCallout: 'none',
